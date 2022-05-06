84dcadd8902086c6952421e09e0d5575d681318a
dima92 <dima-tkachenko@mail.ru>
2022-05-06 04:13:48 +0300

    Lesson 09: Fragment & CSS

---
+++ src/09_fragment_&_css/Lesson.jsx
@@ -0,0 +1,23 @@
+import React, {Fragment} from "react";
+import './styles.css';
+
+const styles = {color: 'red', textTransform: 'uppercase'}
+
+const Columns = () => (
+  <Fragment>
+    <td style={styles}>Inline</td>
+    <td className="title">Simple class</td>
+  </Fragment>
+)
+
+const Table = () => (
+  <table>
+    <tbody>
+    <tr>
+      <Columns/>
+    </tr>
+    </tbody>
+  </table>
+)
+
+export default Table
